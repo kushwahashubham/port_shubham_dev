a1f0603fcf207d63ced7ad71962d8bf8087b985e
Shubham Kushwaha <shubham.kushwaha.star@gmail.com>
2024-02-07 23:11:56 +0530

    themeprovider bug fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import React from "react";
 import { ThemeProvider } from "@/components/theme-provider";
-// import NavigationDots from "@/components/NavigationDots";
-// import SocialMedia from "@/components/SocialMedia";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,22 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" className=" !scroll-smooth">
       <body className={`${inter.className} bg-white dark:bg-black`}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {/* <div>
-            <div className=" fixed bottom-0 left-0 z-50 pb-8 pl-8 max-ds:pb-4 max-ds:pl-4">
-              <SocialMedia />
-            </div> */}
-          {children}
-          {/* <div className="fixed right-0 top-[50%] mr-2 translate-x-0 translate-y-[-50%] max-ds:hidden">
-              <NavigationDots />
-            </div>
-          </div> */}
-        </ThemeProvider>
+        <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
   );
