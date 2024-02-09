e86a5473391507190885dafbcce219ac5c6f5446
Shubham Kushwaha <shubham.kushwaha.star@gmail.com>
2024-02-09 20:02:55 +0530

    skelton component bug fixed

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -1,15 +1,16 @@
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 function Skeleton({
   className,
   ...props
+  // eslint-disable-next-line no-undef
 }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
       className={cn("animate-pulse rounded-md bg-primary/10", className)}
       {...props}
     />
-  )
+  );
 }
 
-export { Skeleton }
+export { Skeleton };
